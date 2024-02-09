4bad09ff2964ff94386ba5b7dc5f9bb3554e184f
Diego Clair <diego93rodrigues@gmail.com>
2024-02-09 00:07:44 -0300

    feat: add unit test for helpers functions

---
+++ internal/frameworks/echo/helpers_test.go
@@ -0,0 +1,184 @@
+package echo
+
+import (
+	"testing"
+
+	"github.com/diegoclair/goswag/internal/generator"
+)
+
+func Test_getFuncName(t *testing.T) {
+	type args struct {
+		name string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "Should return the function name",
+			args: args{name: "github.com/diegoclair/goswag/internal/frameworks/echo.(*Echo).GET-fm"},
+			want: "GET",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getFuncName(tt.args.name); got != tt.want {
+				t.Errorf("getFuncName() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_toGoSwagRoute(t *testing.T) {
+	type args struct {
+		from []*echoRoute
+	}
+	tests := []struct {
+		name string
+		args args
+		want []generator.Route
+	}{
+		{
+			name: "Should return the generator.Route",
+			args: args{from: []*echoRoute{
+				{
+					Route: generator.Route{
+						Method: "GET",
+					},
+				},
+			}},
+			want: []generator.Route{
+				{
+					Method: "GET",
+				},
+			},
+		},
+		{
+			name: "Should return the generator.Route for multiple routes",
+			args: args{from: []*echoRoute{
+				{
+					Route: generator.Route{
+						Method: "GET",
+					},
+				},
+				{
+					Route: generator.Route{
+						Method: "POST",
+					},
+				},
+			}},
+			want: []generator.Route{
+				{
+					Method: "GET",
+				},
+				{
+					Method: "POST",
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := toGoSwagRoute(tt.args.from); len(got) != len(tt.want) {
+				t.Errorf("toGoSwagRoute() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_toGoSwagGroup(t *testing.T) {
+	type args struct {
+		from []*echoGroup
+	}
+	tests := []struct {
+		name string
+		args args
+		want []generator.Group
+	}{
+		{
+			name: "Should return the generator.Group",
+			args: args{from: []*echoGroup{
+				{
+					groupName: "group",
+					routes: []*echoRoute{
+						{
+							Route: generator.Route{
+								Method: "GET",
+							},
+						},
+					},
+					groups: []*echoGroup{
+						{
+							groupName: "subgroup",
+							routes: []*echoRoute{
+								{
+									Route: generator.Route{
+										Method: "POST",
+									},
+								},
+							},
+						},
+					},
+				},
+			}},
+			want: []generator.Group{
+				{
+					GroupName: "group",
+					Routes: []generator.Route{
+						{
+							Method: "GET",
+						},
+					},
+					Groups: []generator.Group{
+						{
+							GroupName: "subgroup",
+							Routes: []generator.Route{
+								{
+									Method: "POST",
+								},
+							},
+							Groups: nil,
+						},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := toGoSwagGroup(tt.args.from); len(got) != len(tt.want) {
+				t.Errorf("toGoSwagGroup() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_getPathParams(t *testing.T) {
+	type args struct {
+		path string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "Should return the path params",
+			args: args{path: "/user/:id"},
+			want: []string{"id"},
+		},
+		{
+			name: "Should return the path params for multiple params",
+			args: args{path: "/user/:id/:name"},
+			want: []string{"id", "name"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getPathParams(tt.args.path); len(got) != len(tt.want) {
+				t.Errorf("getPathParams() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
--- internal/frameworks/gin/helpers_test.go
+++ internal/frameworks/gin/helpers_test.go
@@ -1,8 +1,10 @@
 package gin
 
 import (
+	"reflect"
 	"testing"
 
+	"github.com/diegoclair/goswag/internal/generator"
 	"github.com/gin-gonic/gin"
 )
 
@@ -26,11 +28,196 @@ func TestGetFuncName(t *testing.T) {
 			},
 			want: "handler3",
 		},
+		{
+			name: "Should return the function name of the last handler",
+			args: args{
+				handlers: []gin.HandlerFunc{handler1, handler2},
+			},
+			want: "handler2",
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := getFuncName(tt.args.handlers...); got != tt.want {
 				t.Errorf("getFuncName() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGetPathParams(t *testing.T) {
+	type args struct {
+		path string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "Should return the path parameters",
+			args: args{
+				path: "/path/:id",
+			},
+			want: []string{"id"},
+		},
+		{
+			name: "Should return the path parameters",
+			args: args{
+				path: "/path/:id/:name",
+			},
+			want: []string{"id", "name"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getPathParams(tt.args.path); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("getPathParams() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestToGoSwagRoute(t *testing.T) {
+	type args struct {
+		from []*ginRoute
+	}
+	tests := []struct {
+		name string
+		args args
+		want []generator.Route
+	}{
+		{
+			name: "Should return the generator.Route",
+			args: args{from: []*ginRoute{
+				{
+					Route: generator.Route{
+						Method: "GET",
+					},
+				},
+			}},
+			want: []generator.Route{
+				{
+					Method: "GET",
+				},
+			},
+		},
+		{
+			name: "Should return the generator.Route for multiple routes",
+			args: args{from: []*ginRoute{
+				{
+					Route: generator.Route{
+						Method: "GET",
+					},
+				},
+				{
+					Route: generator.Route{
+						Method: "POST",
+					},
+				},
+			}},
+			want: []generator.Route{
+				{
+					Method: "GET",
+				},
+				{
+					Method: "POST",
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := toGoSwagRoute(tt.args.from); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("toGoSwagRoute() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestToGoSwagGroup(t *testing.T) {
+	type args struct {
+		from []*ginGroup
+	}
+	tests := []struct {
+		name string
+		args args
+		want []generator.Group
+	}{
+		{
+			name: "Should return the generator.Group",
+			args: args{from: []*ginGroup{
+				{
+					groupName: "group1",
+					routes: []*ginRoute{
+						{
+							Route: generator.Route{
+								Method: "GET",
+							},
+						},
+					},
+				},
+			}},
+			want: []generator.Group{
+				{
+					GroupName: "group1",
+					Routes: []generator.Route{
+						{
+							Method: "GET",
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "Should return the generator.Group for multiple groups",
+			args: args{from: []*ginGroup{
+				{
+					groupName: "group1",
+					routes: []*ginRoute{
+						{
+							Route: generator.Route{
+								Method: "GET",
+							},
+						},
+					},
+				},
+				{
+					groupName: "group3",
+					routes: []*ginRoute{
+						{
+							Route: generator.Route{
+								Method: "PUT",
+							},
+						},
+					},
+				},
+			}},
+			want: []generator.Group{
+				{
+					GroupName: "group1",
+					Routes: []generator.Route{
+						{
+							Method: "GET",
+						},
+					},
+				},
+				{
+					GroupName: "group3",
+					Routes: []generator.Route{
+						{
+							Method: "PUT",
+						},
+					},
+					Groups: nil,
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := toGoSwagGroup(tt.args.from); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("toGoSwagGroup() = %v, want %v", got, tt.want)
 			}
 		})
 	}
